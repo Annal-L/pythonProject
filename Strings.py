16187e8e5522b1ec51a773206dae33fc39e027dc
Annal-L <annal.arun19@gmail.com>
2020-10-12 16:34:26 +0530

    added few code snippets

---
+++ Strings.py
@@ -0,0 +1,14 @@
+# String Literals
+# assign string to a variable
+a = "Hello"
+print(a)
+# multiline strings
+a = """Python is a general-purpose programming language, 
+so it can be used for many things.."""
+print(a)
+# three single quotes
+a = ''' Python is used for web development, 
+AI, machine learning, operating systems, 
+mobile application development, and video games.'''
+print(a)
+
