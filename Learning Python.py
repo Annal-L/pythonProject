7f0e220a7e5da6a1982ee737599c371d2cb7e57f
Annal-L <annal.arun19@gmail.com>
2020-10-12 16:19:05 +0530

    added few code snippets

--- Learning Python.py
+++ Learning Python.py
@@ -44,3 +44,64 @@ print('Floor Division of ',n1 ,'and' ,n2 ,'is :',floor_div)
 print('Exponent of ',n1 ,'and' ,n2 ,'is :',power)
 print('Modulus of ',n1 ,'and' ,n2 ,'is :',modulus)
 print('\n')
+
+# same value to multiple variables in one line
+x = y = z = "Python"
+print(x)
+print(y)
+print(z)
+print('\n')
+
+# + character to add a variable to another variable
+x = "Python is "
+y = "awesome"
+z = x + y
+print(z)
+print('\n')
+
+# Global variable with my function
+x = "Python"
+
+def myfunc():
+ x = "fantastic"
+ print("Python is " + x)
+
+myfunc()
+print(x + " is awesome")
+
+# Numbers: Convert from one type to another
+x = 10   # int
+y = 2.9  # float
+z = 5j   # complex
+
+#convert from int to float:
+a = float(x)
+
+#convert from float to int:
+b = int(y)
+
+#convert from int to complex:
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print('\n')
+
+# Type casting
+a = int(2)   # a will be 2
+b = int(9.8) # b will be 9
+c = int("7") # c will be 7
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+i = str("s1") # i will be 's1'
+j = str(2)    # j will be '2'
+k = str(3.0)  # k will be '3.0'
