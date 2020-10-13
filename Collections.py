7ece31d8af58941446c056d9feb618e21ec613a1
Annal-L <annal.arun19@gmail.com>
2020-10-13 10:25:19 +0530

    added some codes for collection (learning)

---
+++ Collections.py
@@ -0,0 +1,23 @@
+# Arrays, List, Tuple, Set, Dictionary
+# Creating a List
+List = []
+print("Blank List: ")
+print(List)
+
+# Creating a List of numbers
+List = [66, 71, 98]
+print("\nList of numbers: ")
+print(List)
+
+# Creating a List of strings and accessing
+# using index
+List = ["Java", "Python", "C++"]
+print("\nList Items: ")
+print(List[0])
+print(List[2])
+
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List = [['Python', 'is'], ['awesome']]
+print("\nMulti-Dimensional List: ")
+print(List)
